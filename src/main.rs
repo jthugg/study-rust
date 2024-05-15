e7577f9e2796f3f77ed01408ef3a5768afc9c08a
Hi. I'm Neo <jthug@naver.com>
2024-05-15 20:07:28 +0900

    log: 스택, 힙, 포인터

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 100;
+    let b = Box::new(a);
+    let c = b.as_ref();
+    let d = &b;
+
+    println!("a + *b = {}", a + *b);
+    println!("변수 b의 주소: {:p}", &b); // 스택 메모리 주소 출력
+    println!("변수 c의 주소: {:p}", &c); // 스택 메모리 주소 출력
+    println!("변수 b가 참조하는 주소: {:p}", b); // 힙 메모리 주소 출력
+    println!("변수 c가 참조하는 주소: {:p}", c); // 힙 메모리 주소 출력
+    println!("변수 d의 값: {}", d);
+    println!("변수 d의 주소: {:p}", &d); // 스택 메모리 주소 출력
+    println!("변수 d가 참조하는 주소: {:p}", d); // 스택 메모리 주소 출력
 }
